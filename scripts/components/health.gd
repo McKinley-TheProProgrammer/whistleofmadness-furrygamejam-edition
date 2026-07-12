ada2dac6150f80aefaab3ceef087208d1628f89c
McKinley <caiohudson.red4@hotmail.com>
2026-07-11 22:05:13 -0300

    feat: Health Component

---
+++ scripts/components/health.gd
@@ -0,0 +1,24 @@
+extends Node
+class_name Health
+
+@export var maxHP : int
+
+signal on_damage_taken(amount : int, hpAfter : int)
+signal on_hp_recovered(amount : int, hpAfter : int)
+
+var currentHP : int
+var isDead : bool
+
+func _ready() -> void:
+	currentHP = maxHP
+
+func take_damage(amount : int) -> void:
+	currentHP = max(currentHP - amount, 0)
+	on_damage_taken.emit(amount, currentHP)
+	if currentHP <= 0:
+		isDead = true
+	 
+
+func recover(amount : int) -> void:
+	currentHP = min(currentHP + amount, maxHP)
+	on_hp_recovered.emit(amount, currentHP)
